a9184cceb6c1570a198be046481d216fcc5b7938
REC <REC@REC-HP8570WLT.swc.com>
2014-07-12 17:50:57 -0500

    Working locally, now working on making it work on the server

---
+++ deploy to openshift.bat
@@ -0,0 +1,5 @@
+c:
+cd \Users\rec\git\skunkworks-mvc\target
+scp -i c:\users\rec\.ssh\default.ppk  cloud-1.0.0-BUILD-SNAPSHOT.war 536e99a6e0b8cd928a00050c@services-fourstrategery.rhcloud.com:/var/lib/openshift/536e99a6e0b8cd928a00050c/app-root/data
+cd ..
+pause
--- src/main/java/net/fourstrategery/cloud/registration/RegistrationController.java
+++ src/main/java/net/fourstrategery/cloud/registration/RegistrationController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import fi.foyt.foursquare.api.FoursquareApi;
 import fi.foyt.foursquare.api.Result;
 import fi.foyt.foursquare.api.entities.CompleteUser;
-import fi.foyt.foursquare.api.entities.Contact;
 
 @Controller
 public class RegistrationController {
@@ -29,12 +29,11 @@ public class RegistrationController {
 	private FourSquareApiFactory fourSquareApiFactory;
 	
 	@RequestMapping(value = "/reg/registration", method = RequestMethod.GET)
-	public String registrationInit() {
-		return "reg/registration";
+	public String registrationInit() {		return "reg/registration";
 	}
 	
 	@RequestMapping(value = "/reg/registrationRedirect", method  = RequestMethod.GET) 
-	public String registrationRedirectedFrom4Sq(@RequestParam(value = "code", required = true) String codeFrom4Sq) {
+	public String registrationRedirectedFrom4Sq(Model model,@RequestParam(value = "code", required = true) String codeFrom4Sq) {
 		
 		logger.debug("Got code [" + codeFrom4Sq + "]");
 		
@@ -61,6 +60,8 @@ public class RegistrationController {
 			player.setFirstName(user.getFirstName());
 			
 			player = playerRepository.save(player);
+			
+			model.addAttribute("user", player);
 			
 			logger.debug("Inserted new Player ID=" + player.getId());
 			
